e02f5d4c811a4f3b02202dc3872075a24f6204ad
Juanequex <ramon.g.uz37@gmail.com>
2023-09-20 16:39:05 -0600

    fix permited params and schema record

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @articles = Article.all
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
 
     redirect_to root_path, status: :see_other
   end
-
+  
   private
 
   def article_params
--- db/schema.rb
+++ db/schema.rb
@@ -40,10 +40,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_17_055104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", default: "", null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
